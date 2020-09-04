da0a16ab5413afc2bc2cf31ddf7cdddd50a088e5
jameslo1212 <jameslo1212@hotmail.com>
2020-09-04 19:04:59 +0800

    2020Sep4

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name='Nobody'):
+    return f'hello:{name} '
+
+@app.route('/hi')
+@app.route('/hi/<name>')
+def hi(name='Nobody'):
+    list=['python','java','C++']
+    return render_template('hi.html',name=name,list=list)
+
+
+if __name__ == "__main__":
+    app.run()
